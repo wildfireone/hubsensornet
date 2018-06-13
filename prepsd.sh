bed9221a0a2bfb292574c9632592717f4944be05
decantr <theeasterlywood@gmail.com>
2018-06-13 14:55:36 +0100

    copy setup to sdcard

--- prepsd.sh
+++ prepsd.sh
@@ -12,14 +12,14 @@ fi
 sudo dd if=~/2018-04-18-raspbian-stretch-lite.img bs=4M of=/dev/mmcblk0 status=progress && sync
 
 #
-if [ ! -d /mnt/sd ]; then
-    sudo mkdir /mnt/sd/boot
-    sudo mkdir /mnt/sd/root
-fi
+sudo mkdir -p /mnt/sd/boot
+sudo mkdir /mnt/sd/root
+
 sudo mount /dev/mmcblk0p1 /mnt/sd/boot
 sudo mount /dev/mmcblk0p2 /mnt/sd/root
 
 sudo touch /mnt/sd/boot/ssh
 
 sudo echo -e "if [ -e /setup.sh ]; then sudo bash /setup.sh $1 && sudo rm /setup.sh; fi" >> /mnt/sd/root/etc/rc.local
+sudo cp ./setup.sh /mnt/sd/root/
 
