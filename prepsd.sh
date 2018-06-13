17bc3622d94b5efd51fd1e5b7e726915abc6a81e
decantr <theeasterlywood@gmail.com>
2018-06-13 15:28:43 +0100

    removed checking it wasn't working

--- prepsd.sh
+++ prepsd.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if [ ! -a "*-raspbian-stretch-lite.img" ]; then
-    if [ -a "*-raspbian-stretch-lite.zip" ]; then
-        unzip "*-raspbian-stretch-lite.zip"
-    else
-        wget -q --show-progress https://downloads.raspberrypi.org/raspbian_lite_latest
-        unzip "*-raspbian-stretch-lite.zip"
-    fi
-fi
+#echo "checking for image file in current dir"
+#if [ ! -a "*-raspbian-stretch-lite.img" ]; then
+#    if [ -a "*-raspbian-stretch-lite.zip" ]; then
+#        echo "zip found, unpacking"
+#        unzip "*-raspbian-stretch-lite.zip"
+#    else
+#        echo "zip not found getting from raspberrypi.org"
+wget -q --show-progress --content-disposition "https://downloads.raspberrypi.org/raspbian_lite_latest"
+unzip 2018-04-18-raspbian-stretch-lite.zip
+#    fi
+#fi
 
 if ! lsblk | grep -q mmcblk0 ; then
     echo "no sd card found please plug one in"
     exit 1
 fi
 
-echo -e "this will erase all data on the sd card, are you sure?\n"
+echo -e "this will erase all data on the sd card, are you sure?"
 read -p "Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo dd if="*-raspbian-stretch-lite.img" bs=4M of=/dev/mmcblk0 status=progress && sync
