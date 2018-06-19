a5491125fa50fe5db16b59b8abd2d137e6b83200
decantr <theeasterlywood@gmail.com>
2018-06-19 13:47:04 +0100

    add numbering to sd creation

--- prepsd.sh
+++ prepsd.sh
@@ -2,9 +2,17 @@
 
 if [ "$1" = "1" ]; then
     a=$1
+    name="pihost"
+    elif [ "$1" = "0" ]; then
+    a=$1
+    if [ -z "$2" ]; then
+        echo "please supply a number for the pi"
+        elif [[ $(($2)) != $2 ]]; then
+        name=pitest$2
+    fi
 else
     echo "no choice made, defaulting to client(0)"
-    a=0
+    exit 1
 fi
 
 if [ ! -f 2018-04-18-raspbian-stretch-lite.img ]; then
@@ -43,6 +51,8 @@ sleep 1
 echo -e "moving files\\n"
 sudo touch /mnt/sd/boot/ssh
 sudo sed -i "\$iif [ -e /setup.sh ]; then sudo bash /setup.sh $a && sudo rm /setup.sh && sudo reboot; fi" /mnt/sd/root/etc/rc.local
+echo "$name" | sudo tee /etc/hostname > /dev/null
+sudo sed -i "s/raspberrypi/$name/" /etc/hosts
 sudo cp ./setup.sh /mnt/sd/root/
 
 sleep 1
