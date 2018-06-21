cb41d88e03ea019258e28cbf3ff1c46ab5c676de
decantr <theeasterlywood@gmail.com>
2018-06-21 16:45:13 +0100

    added sensor reading and made sure it was still portable

--- read.py
+++ read.py
@@ -5,11 +5,16 @@ from time import sleep
 from socket import gethostname
 from re import search
 
+p, s = None, None
+
 # determine endpoint based on assumed net topology
 if (gethostname() == "pihost"):
     end = "localhost"
 elif (search(r'pitest\d', gethostname(), 0)):
     end = "pihost.local"
+    from Adafruit_DHT import DHT11, read_retry
+    s = DHT11
+    p = 4
 else:
     end = "pihost"
 
@@ -17,6 +22,21 @@ else:
 db = InfluxDBClient(host=end, database="test")
 
 while True:
+    h, t = None, None
+    if s is not None:
+        h, t = read_retry(s, p)
+
+    if h is not None and t is not None:
+        # print('temp={0:0.1f}*C humid={1:0.1f}%'.format(t, h))
+        msg1 = [{
+            'measurement': 'atmos',
+            'fields': {
+                'host': gethostname(),
+                'temp': t,
+                'humi': h
+            }
+        }]
+        db.write_points(msg1)
     # create json message
     msg = [{
         'measurement': 'usage',
