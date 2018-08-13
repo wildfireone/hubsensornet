9a2c6e60954c9446852d0408d0d2fedb1eec0183
decantr <theeasterlywood@gmail.com>
2018-08-13 02:56:21 +0100

    add option for choosing time period

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const color = (d) => { return d3.scale
-	.linear()
-	.domain([9, 50])
-	.range(["steelblue", "brown"])
-	.interpolate(d3.interpolateLab)(d['time']) }
+const val = document.getElementById('time-increments')
 
-const parcoords = d3
-    .parcoords()('#para')
-    .color(color)
-    .alpha(0.4)
+val.addEventListener('change',function() {
+    build(val.value)
+},false)
 
-d3.json('http://pihost:3000/data?time=1d', (data) => {
-  parcoords
-    .data(data)
-    .hideAxis(["time", "host"])
-    .composite("darker")
-    .render()
-    .shadows()
-    .reorderable()
-    .brushMode("1D-axes")
-})
+function build(time) {
+    console.log(time)
+    const color = (d) => { return d3.scale
+        .linear()
+        .domain([9, 50])
+        .range(["steelblue", "brown"])
+        .interpolate(d3.interpolateLab)(d['time']) }
+
+    const parcoords = d3
+        .parcoords()('#para')
+        .color(color)
+        .alpha(0.4)
+
+    d3.json('http://pihost:3000/data?time=' + time, (data) => {
+    parcoords
+        .data(data)
+        .hideAxis(["time", "host"])
+        .composite("darker")
+        .render()
+        .shadows()
+        .reorderable()
+        .brushMode("1D-axes")
+    })
+}
