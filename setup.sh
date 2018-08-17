d2664efd9867642ef1323f626ac4edaf77c4e190
decantr <theeasterlywood@gmail.com>
2018-08-17 17:22:49 +0100

    move to python2

--- setup.sh
+++ setup.sh
@@ -13,15 +13,15 @@ done
 
 function client {
     echo "installing python"
-    sudo apt install python3 python3-pip -y
-    sudo pip3 install --upgrade pip
+    sudo apt install python-pip -y
+    sudo pip install --upgrade pip
     echo "installing python libs"
-    sudo pip3 install influxdb psutil Adafruit_DHT
+    sudo pip install influxdb psutil Adafruit_DHT
     
     echo "getting test script"
     curl -fsSL https://raw.githubusercontent.com/decantr/hubsensorcode/master/read.py | sudo tee /read.py > /dev/null
     echo "adding test script to autorun"
-    sudo sed -i '$ipython3 /read.py &' /etc/rc.local
+    sudo sed -i '$ipython /read.py &' /etc/rc.local
 }
 
 function server {
