503b49ef45d63bb0208e2be917291fcb83237deb
decantr <theeasterlywood@gmail.com>
2018-08-21 16:36:32 +0100

    fix the internet check

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 net=false
-while $net ; do
+until $net ; do
     wget -q --spider www.google.com
-    if [ $? -eq 1 ]; then
-        echo "no internet waiting 5 then trying again"
-        sleep 5
+    if [ $? -eq 0 ]; then
+      net=true
     else
-        net=true
+      echo "no internet waiting 5 before trying again"
+      sleep 5
     fi
 done
 
@@ -17,7 +17,7 @@ function client {
     sudo pip install --upgrade pip
     echo "installing python libs"
     sudo pip install influxdb psutil Adafruit_DHT
-    
+
     echo "getting test script"
     curl -fsSL https://raw.githubusercontent.com/decantr/hubsensorcode/master/read.py | sudo tee /read.py > /dev/null
     echo "adding test script to autorun"
@@ -28,11 +28,11 @@ function server {
     echo "installing docker"
     curl -fsSL get.docker.com | sudo sh
     sudo usermod -aG docker pi
-    
+
     docker create --name pidb --restart=always --network pinet -p 8086:8086 influxdb
-    
+
     docker start pidb
-    
+
     sleep 10
     curl -G 'http://localhost:8086/query' --data-urlencode "q=create database test"
 }
