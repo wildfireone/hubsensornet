4fa3970779260ae9ebaf800266de62885cec22bb
decantr <theeasterlywood@gmail.com>
2018-08-24 17:46:52 +0100

    fix node install

--- lib/setup.sh
+++ lib/setup.sh
@@ -25,20 +25,29 @@ function client {
 }
 
 function server {
+	
+	sleep 30
+
 	echo "installing docker"
     curl -fsSL get.docker.com | sudo sh
+
+	sudo touch /setup.sh.log
+	echo "installing docker" | sudo tee -a /setup.sh.log
+	curl -kfsSL get.docker.com | sudo bash > /setup.docker.log
+	echo "docker installed " | sudo tee -a /setup.sh.log
 	sudo usermod -aG docker pi
 
-	echo "creating pidb container"
-	docker create --name pidb --restart=always -p 8086:8086 influxdb
-	docker start pidb
+	echo "creating pidb container" | sudo tee -a /setup.sh.log
+	sudo docker create --name pidb --restart=always -p 8086:8086 influxdb
+	sudo docker start pidb
 
 	# TODO turn this into a node package
-	echo "getting server files"
+	echo "getting server files" | sudo tee -a /setup.sh.log
 	sudo mkdir /server
-    curl -fsSL https://raw.githubusercontent.com/decantr/hubsensorcode/master/server.js | sude tee /server/server.json > /dev/null
+	git clone https://github.com/decantr/hubsensorui.git /server 
+	curl -fsSL https://raw.githubusercontent.com/decantr/hubsensorcode/master/server.js | tee /server/server.js > /dev/null
 
-	echo "installing node"
+	echo "installing node" | sudo tee -a /setup.sh.log
 	cd /tmp
 	wget https://nodejs.org/dist/v10.8.0/node-v10.8.0-linux-armv7l.tar.xz
 	tar xf node-v10.8.0-linux-armv7l.tar.xz 
@@ -46,16 +55,20 @@ function server {
 	sudo mv bin/* /bin/
 	sudo mv lib/* /lib/
 	sudo mv share/* /usr/share/
+
+	echo "install dependencies" | sudo tee -a /setup.sh.log
+	cd /server
+	npm i -s express http-server
 	cd ~
-	echo "install dependencies"
-	sudo npm i -g express http-server
+
+	sudo chown -R pi:pi /server
 
 	echo "adding server to autorun"
-	sudo sed -i '$inode /server.js &' /etc/rc.local
-	sudo sed -i '$http-server /server &' /etc/rc.local
+	sudo sed -i '$inode /server/server.js &' /etc/rc.local
+	sudo sed -i '$i/server/node_modules/http-server/bin/http-server /server &' /etc/rc.local
 
 	sleep 10
-	curl -G 'http://localhost:8086/query' --data-urlencode "q=create database test"
+	curl -XPOST 'http://localhost:8086/query' --data-urlencode "q=create database test"
 }
 
 if [ "$1" == "0" ]; then
